5cec3aa68b3cb2f5df63f1c9f5c9c88f5152888e
Luthfi Solahudin <luthfisolahudin@outlook.com>
2025-10-07 17:46:55 +0700

    refactor: add firebase config

--- activation.js
+++ activation.js
@@ -980,7 +980,15 @@ class MockActivationService {
 
 (async () => {
   try {
-    const config = window.ACTIVATION_APP_CONFIG || {};
+    const firebaseConfig = {
+      apiKey: 'AIzaSyB9_J1AZkSbCM9v3PeV4m33qojHX51bLwg',
+      authDomain: 'finalytics-62350.firebaseapp.com',
+      projectId: 'finalytics-62350',
+      storageBucket: 'finalytics-62350.firebasestorage.app',
+      messagingSenderId: '586305419053',
+      appId: '1:586305419053:web:b94a325fd5b649340305a4',
+    }
+    const config = window.ACTIVATION_APP_CONFIG || { firebaseConfig };
     activationService = await ActivationServiceFactory.create(config);
     setState({ ready: true });
   } catch (error) {
